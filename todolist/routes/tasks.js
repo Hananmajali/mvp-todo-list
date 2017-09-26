dc71a16e3e8d2b234a16cb5eae7b9ab9f3a07e24
hananmajali <hananmajali@gmail.com>
2017-09-26 15:50:16 -0400

    git the api tasks from mlab by using mongojs

--- todolist/routes/tasks.js
+++ todolist/routes/tasks.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var mongojs= require('mongojs');
+var db = mongojs('mongodb://hanan:Hanan@ds149324.mlab.com:49324/hananmajalidatabase',['tasks'])
+
+// router.get('/tasks', function(req,res,next){
+// 	res.send('tasks api page \__(^_*)__/ ')
+// })
 
 router.get('/tasks', function(req,res,next){
-	res.send('tasks api page \__(^_*)__/ ')
+	db.tasks.find(function(err,tasks){
+		if (err){
+			res.send("there is an err in database"+ err);
+		}
+		    res.json(tasks);
+	})
+})
+
+router.get('/task/:id', function(req,res,next){
+	//var objectID = require("mongojs").ObjectId("objectidhere");
+	db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)},function(err,task){
+		if (err){
+			res.send("there is an err in database"+ err);
+		}
+		    res.json(task);
+	})
 })
 
 module.exports= router;
