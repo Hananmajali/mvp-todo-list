03ee5a3c6b3b8a04caf4aadf731995ac0e720140
hananmajali <hananmajali@gmail.com>
2017-09-26 14:16:35 -0400

    intalizing index.html page

--- todolist/routes/index.js
+++ todolist/routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+// router.get('/', function(req,res,next){
+// 	res.send('index  \__(^_*)__/')
+// })
+
 router.get('/', function(req,res,next){
-	res.send('index  \__(^_*)__/')
+	res.render('index.html')
 })
 
 module.exports= router;
