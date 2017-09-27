72664941a877b885603a69138fbe634d2200c0b8
hananmajali <hananmajali@gmail.com>
2017-09-27 09:50:42 -0400

    todolist with out connection

--- todolist/routes/tasks.js
+++ todolist/routes/tasks.js
@@ -9,6 +9,7 @@ var db = mongojs('mongodb://hanan:Hanan@ds149324.mlab.com:49324/hananmajalidatab
 
 router.get('/tasks', function(req,res,next){
 	db.tasks.find(function(err,tasks){
+		//console.log(tasks) --> its an array of my tasks
 		if (err){
 			res.send("there is an err in database"+ err);
 		}
@@ -24,6 +25,67 @@ router.get('/task/:id', function(req,res,next){
 		}
 		    res.json(task);
 	})
+})
+
+
+//handle save task
+
+router.post('/task', function(req,res,next){
+	//console.log(req.body)--> it will show the task
+	var task = req.body
+	if(!task.task || (task.isComplete + '')){
+		res.status(400)
+		res.json({
+			"err": "not good data"
+		});
+
+	} else{ // every thing fine
+		db.tasks.save(task, function(err, task){
+			if(err){
+				res.send(err)
+			}
+			res.json(task);
+		})
+	}	
+})
+
+// delete a task --> it will be depend on the spicific id of those tasks
+// so its the same of get one task but with delete propertty 
+
+router.delete('/task/:id', function(req,res,next){
+	//var objectID = require("mongojs").ObjectId("objectidhere");
+	db.tasks.remove({_id: mongojs.ObjectId(req.params.id)},function(err,task){
+		if (err){
+			res.send("there is an err in database"+ err);
+		}
+		    res.json(task);
+	})
+})
+
+router.put('/task/:id', function(req,res,next){
+	var task = req.body;
+	var updat = {};
+
+	if(task.isComplete){
+		updat.isComplete= task.isComplete;
+	}
+
+	if(task.task){
+		updat.task= task.task;
+	}
+	if(!updat){
+		res.status(400)
+		res.json({
+			"err":"error"
+		})
+	} else {
+		db.tasks.update({_id: mongojs.ObjectId(req.params.id)},function(err,task){
+		if (err){
+			res.send("there is an err in database"+ err);
+		}
+		    res.json(task);
+	});
+	}
 })
 
 module.exports= router;
